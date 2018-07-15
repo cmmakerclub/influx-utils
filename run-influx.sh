84c5046dd89539b0b291031a96381cb66a52d94e
Nat <nat@cmmc.io>
2018-07-15 21:50:40 +0700

    add docker running container checking

--- run-influx.sh
+++ run-influx.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 VERSION=1.0.0
 usage() {
         cat <<EOF
@@ -19,41 +20,40 @@ confirm() {
             false
             ;;
     esac
-} 
+}
 
 setup() {
         cat <<EOF
 $0 v$VERSION
 EOF
   if [ -z "$CONTAINER_NAME" ]; then
-    while True; do
        read -r -p "Enter CONTAINER_NAME (influxdb): " CONTAINER_NAME
        if [ -z "$CONTAINER_NAME" ]; then
           CONTAINER_NAME=influxdb
        fi
-       docker inspect "${CONTAINER_NAME}" &> /dev/null
-    done;
+       #docker inspect $CONTAINER_NAME 2> /dev/null
+       #echo $?
   fi
-  
+
   ROOT_DIR="${ROOT_DIR:-$HOME/tick/influxdb}"
   DATA_PATH="${DATA_PATH:-${ROOT_DIR}/data}"
   INFLUX_PORT="${INFLUX_PORT:-8086}"
   INFLUXDB_BIND_ADDRESS="${INFLUXDB_BIND_ADDRESS:-8088}"
   INFLUX_CONF="${INFLUX_CONF:-$ROOT_DIR/influxdb.conf}"
   DOCKER_NETWORK="${DOCKER_NETWORK:-host}"
-  
+
   echo "CONTAINER_NAME=$CONTAINER_NAME"
   echo "ROOT_DIR=$ROOT_DIR"
   echo "DATA_PATH=$DATA_PATH"
   echo "INFLUX_CONF=$INFLUX_CONF"
-  echo "INFLUX_PORT=$INFLUX_PORT" 
-  echo "INFLUXDB_BIND_ADDRESS=$INFLUXDB_BIND_ADDRESS" 
+  echo "INFLUX_PORT=$INFLUX_PORT"
+  echo "INFLUXDB_BIND_ADDRESS=$INFLUXDB_BIND_ADDRESS"
   echo "DOCKER_NETWORK=$DOCKER_NETWORK"
-  
+
   if confirm ;then
     mkdir -p $DATA_PATH
-    echo "data/" > "$ROOT_DIR/.gitignore" 
-    echo "meta/" >> "$ROOT_DIR/.gitignore" 
+    echo "data/" > "$ROOT_DIR/.gitignore"
+    echo "meta/" >> "$ROOT_DIR/.gitignore"
     docker run --rm influxdb influxd config > $INFLUX_CONF
     sed -Ei "s/auth-enabled = false/auth-enabled = true/g" $INFLUX_CONF
     docker run \
@@ -66,6 +66,12 @@ EOF
           -d \
           --name "${CONTAINER_NAME}" \
           influxdb -config /etc/influxdb/influxdb.conf
+
+          if [ $? -ne 0 ]; then
+            echo "setup failed."
+            exit -1
+          fi
+
           hash influx &> /dev/null
           if [ $? -eq 1 ]; then
               sudo apt-get install -y influxdb-client
@@ -80,9 +86,9 @@ EOF
                read -r -p "Enter INFLUX_ADMIN_PASSWORD: " INFLUX_ADMIN_PASSWORD
           done
 
-          echo "INFLUX_ADMIN_USER=$INFLUX_ADMIN_USER" 
-          echo "INFLUX_ADMIN_PASSWORD=$INFLUX_ADMIN_PASSWORD" 
-          influx -execute "CREATE USER \"${INFLUX_ADMIN_USER}\" WITH PASSWORD '${INFLUX_ADMIN_PASSWORD}' WITH ALL PRIVILEGES" 
+          echo "INFLUX_ADMIN_USER=$INFLUX_ADMIN_USER"
+          echo "INFLUX_ADMIN_PASSWORD=$INFLUX_ADMIN_PASSWORD"
+          influx -execute "CREATE USER \"${INFLUX_ADMIN_USER}\" WITH PASSWORD '${INFLUX_ADMIN_PASSWORD}' WITH ALL PRIVILEGES"
   fi
 }
 
