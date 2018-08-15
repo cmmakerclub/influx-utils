fa79a14d6dc401211ac3145513fb4289af242ff4
Nat Weerawan <nat@cmmc.io>
2018-08-15 22:54:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ~
-curl https://github.com/cmmakerclub/influx-utils/blob/master/run-influx.sh > cmmc-influx.sh
+curl https://raw.githubusercontent.com/cmmakerclub/influx-utils/master/run-influx.sh > cmmc-influx.sh
 chmod +x cmmc-influx.sh
 sudo mv cmmc-influx.sh /usr/bin/cmmc-influx
