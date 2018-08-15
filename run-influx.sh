66433d238f0fbddc3a37da71772b23ff06d348c0
Nat Weerawan <nat@cmmc.io>
2018-08-15 23:49:44 +0700

    Update run-influx.sh

--- run-influx.sh
+++ run-influx.sh
@@ -132,7 +132,7 @@ createdb() {
 }
 
 run_grafana() {
-  DMIN_PASSWORD="secret"
+  ADMIN_PASSWORD="secret"
   NAME="grafana"
   docker volume create grafana-storage 
   docker run -d -p 3001:3000 -e "GF_SECURITY_ADMIN_PASSWORD=${ADMIN_PASSWORD}" --name="${NAME}" -v grafana-storage:/var/lib/grafana  grafana/grafana
