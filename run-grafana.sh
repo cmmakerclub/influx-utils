08e439432ae5818e530b8838228db39dd2f2e27a
Nat Weerawan <nat@cmmc.io>
2018-08-15 23:57:16 +0700

    Delete run-grafana.sh

--- run-grafana.sh
+++
@@ -1,4 +0,0 @@
-ADMIN_PASSWORD="secret"
-NAME="grafana"
-docker volume create grafana-storage 
-docker run -d -p 80:3000 -e "GF_SECURITY_ADMIN_PASSWORD=${ADMIN_PASSWORD}" --name="${NAME}" -v grafana-storage:/var/lib/grafana  grafana/grafana
