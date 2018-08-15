ceeb575204ad9c1ee43d3bacbca8d354ae02cdb4
Nat <nat@cmmc.io>
2018-08-15 22:53:13 +0700

    change cmmc->cmmc-influx

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 cd ~
 curl https://github.com/cmmakerclub/influx-utils/blob/master/run-influx.sh > cmmc-influx.sh
 chmod +x cmmc-influx.sh
-sudo mv cmmc-influx.sh /usr/bin/cmmc
+sudo mv cmmc-influx.sh /usr/bin/cmmc-influx
