add5baa26bde5b879260b818b8212d04c8311b95
Nat Weerawan <nat@cmmc.io>
2018-08-30 11:11:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "installing cmmc-influx-utils v1.0.2"
+echo "installing cmmc-influx-utils v1.0.3"
 
 cd ~
 curl --silent https://raw.githubusercontent.com/cmmakerclub/influx-utils/master/run-influx.sh > cmmc-influx.sh
